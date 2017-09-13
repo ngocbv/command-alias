5a55e1da09bac9aa34f1f08a7c930ff885905254
NgocB <neversmile12b1@gmail.com>
2017-09-13 10:19:57 +0700

    Commit time: 10:18:10 13/09/17 in HNNB.local

--- lib/bashrc/bashrc.rb
+++ lib/bashrc/bashrc.rb
@@ -1,16 +1,24 @@
+require 'etc'
+
 class BashRC
   class << self
     def create_bash_alias
       file_path = File.join(File.dirname(__FILE__), 'helpful_bashrc.txt')
       bashrc = File.read(file_path)
 
-      File.open('~/.bashrc_command_alias', 'w') do |f|
+      home_folder = Etc.getpwuid.dir
+      File.open(File.join(home_folder, '.bashrc_command_alias'), 'w') do |f|
         f.write(bashrc)
       end
+      puts "Created file ~/.bashrc_command_alias"
 
-      File.open('~/.bashrc', 'a') do |f|
+      File.open(File.join(home_folder, '.bashrc'), 'a') do |f|
         f.write("\nsource ~/.bashrc_command_alias")
       end
+      puts "Added 'source ~/.bashrc_command_alias' to ~/.bashrc"
+
+      system("source #{home_folder}/.bashrc")
+      puts "Run 'source #{home_folder}/.bashrc'"
     end
   end
 end
